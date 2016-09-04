18168685076fe4cc57c72afa0f3c3fc3340ecb7e
martykells <martykells@gmail.com>
2016-09-04 20:58:22 +0100

    babel support with simple hello.jsx

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const express = require('express');
+const path = require('path')
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-	res.send('Hello world\n');
-    });
+app.use(express.static('app'));
+app.use('/bootstrap', express.static(path.join(__dirname,"node_modules/bootstrap/dist/")));
+app.use('/babel-core', express.static(path.join(__dirname,"node_modules/babel-core/")));
+app.use('/react', express.static(path.join(__dirname,"node_modules/react/dist/")));
+app.use('/react-dom', express.static(path.join(__dirname,"node_modules/react-dom/dist/")));
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
